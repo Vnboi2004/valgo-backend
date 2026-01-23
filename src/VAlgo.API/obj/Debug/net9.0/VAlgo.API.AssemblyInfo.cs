984a4e91396c86393b891d82ee0fe8a9dc24c453
TruongPhuocHung <insect228060132902060400006417@gmail.com>
2026-01-23 23:26:36 +0700

    meger code feature/problemManagement-infrastructure to develop

--- src/VAlgo.API/obj/Debug/net9.0/VAlgo.API.AssemblyInfo.cs
+++ src/VAlgo.API/obj/Debug/net9.0/VAlgo.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VAlgo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cd9e08e8bc67c9ecc8220ceb65d0620daf79af8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+013a077aa0fd912a537cbbdc1138c7e4547f2cd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("VAlgo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VAlgo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
