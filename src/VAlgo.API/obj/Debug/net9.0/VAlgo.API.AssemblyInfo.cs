8bab3802235c3d27272630e930acdae7ad3a7e3b
TruongPhuocHung <insect228060132902060400006417@gmail.com>
2026-01-25 13:05:20 +0700

    Merge branch from feature/problemClassification-api to develop

--- src/VAlgo.API/obj/Debug/net9.0/VAlgo.API.AssemblyInfo.cs
+++ src/VAlgo.API/obj/Debug/net9.0/VAlgo.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VAlgo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646e31f3494f83dc90e1b115eb792740c8b6ea5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5774350e10884964dbc55de3c2e98f7bb2627052")]
 [assembly: System.Reflection.AssemblyProductAttribute("VAlgo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VAlgo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
